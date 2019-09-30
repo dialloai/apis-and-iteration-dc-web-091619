b36fd9b3760e9b6be76c1769a9bd8b10be974bfb
github username <github email address>
2019-09-30 12:38:59 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,25 +1,43 @@
 require 'rest-client'
 require 'json'
 require 'pry'
-
+require 'pry'
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+  
+  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_hash = JSON.parse(response_string)
+  results = response_hash["results"].select do |info|
+  info["name"].downcase == character_name 
+  end
+  films = results[0]["films"]
+end
+
+  
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  film_hashes = films.map do |film_url|
+    response_string = RestClient.get(film_url)
+    response_hash = JSON.parse(response_string)
+    response_hash
+  end
+  film_hashes.each do |film| 
+   puts  film["title"]
+    # print film["characters"]
+  end
+  # binding.pry
+
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to Star Wars Searchmania!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+character_name = gets.chomp
+return character_name.downcase 
 end
